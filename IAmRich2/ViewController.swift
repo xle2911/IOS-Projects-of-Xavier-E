8ca675c2f1f0a7498934e11234792f6273ce66c8
xavier encarnacion <tesishelp2@gmail.com>
2021-04-07 18:26:28 -0500

    Initial Commit

---
+++ IAmRich2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich2
+//
+//  Created by imac on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
